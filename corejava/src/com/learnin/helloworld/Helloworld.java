8cdb926bb216714e8d2b19dba72516015b2a8338
vinaymachinetest <developer.vinay.28@gmail.com>
2020-04-16 23:51:34 +0530

    modified Helloworld java file

--- corejava/src/com/learnin/helloworld/Helloworld.java
+++ corejava/src/com/learnin/helloworld/Helloworld.java
@@ -3,6 +3,6 @@ package com.learnin.helloworld;
 public class Helloworld {
 	public static void main(String []args) {
 		
-		System.out.println("way to learning java8 progoramming");
+		System.out.println("way to learning java8 progoramming with git");
 	}
 }
